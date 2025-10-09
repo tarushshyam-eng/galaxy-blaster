97744d45d73e01d4dfbdd1b2659aa4819741a6d4
tarushshyam-eng <tarushshyam@gmail.com>
2025-10-09 12:25:56 +0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,61 +1,81 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile2 = sprites.createProjectileFromSprite(img`
-        2 
-        5 
-        5 
-        `, Spaceship, 50, -100)
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 7 . . . . . . . . . 
+        . . . . . 7 . . 7 7 7 7 . . . . 
+        . . . . . . . 7 7 7 7 7 . . . . 
+        . . . . . . 7 7 7 7 7 7 . . . . 
+        . . . . . . 7 7 7 7 7 7 . . . . 
+        . . . . . . 7 7 7 7 7 . . . . . 
+        . . . . . . 7 7 7 7 7 . . . . . 
+        . . . . . . . 7 7 7 . 7 7 . . . 
+        . . . . . . . . . . . 7 . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, Spinjitsu, 50, -100)
+    music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.UntilDone)
 })
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
-    sprites.destroy(Surf_excel, effects.fire, 500)
+    sprites.destroy(sprite, effects.fire, 500)
     info.changeLifeBy(-1)
-    music.play(music.melodyPlayable(music.smallCrash), music.PlaybackMode.UntilDone)
+    music.play(music.melodyPlayable(music.smallCrash), music.PlaybackMode.InBackground)
+})
+info.onLifeZero(function () {
+    game.gameOver(true)
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     sprites.destroy(otherSprite, effects.fire, 500)
+    info.changeScoreBy(1)
 })
-let Surf_excel: Sprite = null
+let Lord_ras: Sprite = null
 let projectile2: Sprite = null
-let Spaceship: Sprite = null
+let Spinjitsu: Sprite = null
 effects.starField.startScreenEffect()
-Spaceship = sprites.create(img`
-    . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . 
-    . . . . . . 8 8 . . . . . . . 
-    . . . . . 8 8 8 8 . . . . . . 
-    . . . . . 1 1 1 1 . . . . . . 
-    . . . 2 2 1 1 1 1 2 2 . . . . 
-    . . 2 2 . 1 5 5 1 . 2 2 . . . 
-    . 2 2 . . 1 5 5 1 . . 2 2 . . 
-    . . . . 2 1 1 1 1 2 . . . . . 
-    . . . . 2 1 1 1 1 2 . . . . . 
-    . . . 2 2 . . . . 2 2 . . . . 
+Spinjitsu = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    7 7 7 7 7 7 f 7 7 f 7 7 7 7 7 7 
+    7 f f 7 7 7 f 7 7 7 7 7 7 f 7 7 
+    7 7 f 7 7 7 7 f 7 7 7 7 f 7 7 7 
+    . 7 7 f 7 7 7 7 f 7 7 f 7 7 7 . 
+    . . 7 7 f 7 7 7 f 7 7 f 7 7 . . 
+    . . . 7 7 f 7 7 7 7 7 7 7 . . . 
+    . . . 7 7 7 f 7 7 f 7 7 . . . . 
+    . . . . 7 7 7 7 f f 7 . . . . . 
+    . . . . 7 f f 7 f 7 7 . . . . . 
+    . . . . . 7 7 f 7 7 . . . . . . 
+    . . . . . 7 f 7 7 . . . . . . . 
+    . . . . . . f 7 7 . . . . . . . 
+    . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-Spaceship.setPosition(84, 98)
-controller.moveSprite(Spaceship)
+Spinjitsu.setPosition(84, 98)
+controller.moveSprite(Spinjitsu)
+tiles.setCurrentTilemap(tilemap`level1`)
 info.setLife(3)
 game.onUpdateInterval(500, function () {
-    Surf_excel = sprites.create(img`
+    Lord_ras = sprites.create(img`
         . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . f . . . . 
-        . . . f . . . . . . f f f . . . 
-        . . f f f . . . . . f . . . . . 
-        . . . . f f . . . f f . . . . . 
-        . . . . . f f . . f . . . . . . 
-        . . . . 9 9 9 9 9 9 9 . . . . . 
-        . . . . 9 7 . . 7 . 9 . . . . . 
-        . . . 9 7 7 f 7 f . . 9 . . . . 
-        . . . 9 . 7 7 7 7 . . 9 . . . . 
-        . . 9 . . 7 f f 7 7 . . 9 . . . 
-        . . 9 . . 7 7 7 7 . . . 9 . . . 
-        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 . 
-        . . 6 6 6 6 6 6 6 6 6 6 6 . . . 
-        . . . . 6 6 6 6 6 6 6 . . . . . 
+        . . . . . f . . . . . f . . . . 
+        . . . . f f f . . . f f f . . . 
+        . . . . f 2 f . . . f 2 f . . . 
+        . . . . f 2 f f f f f 2 f f . . 
+        . . . f 2 2 f 2 2 f 2 2 2 f . 5 
+        . . . f 2 2 2 2 2 2 2 2 2 f 4 5 
+        . . . f f 2 f f f 2 2 2 f 4 5 5 
+        . . . . f f f f f f f f f 5 . . 
+        d . . . . . 2 2 2 2 2 5 4 5 . d 
+        d d f f f f 2 f f f 2 f f f d d 
+        d f f f f f 2 2 2 2 2 f f f f d 
+        . . 4 5 4 . f f . . f . . . . . 
+        . . 5 4 . f f f . . f f . . . . 
+        . . . . . f f f . . f f f . . . 
+        . . . . f f f f . . f f f . . . 
         `, SpriteKind.Enemy)
-    Surf_excel.setPosition(randint(0, scene.screenWidth()), 0)
-    Surf_excel.follow(Spaceship, 50)
+    Lord_ras.setPosition(randint(0, scene.screenWidth()), 0)
+    Lord_ras.follow(Spinjitsu, 50)
 })
---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100001010101010101010101000000000000020c030f03030d0a0b04000000000000090a0707070c07070d050000000000000b05000e0000000f090500000000000009050d00000f0d000b050000000000000f0c00000000000e0905000000000000090b030e0303030c0d0500000000000008070f070b070e0707060000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.builtin.forestTiles0,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundNorthEast0,sprites.dungeon.darkGroundEast,sprites.dungeon.darkGroundSouthEast0,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundSouthWest0,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundCenter,sprites.dungeon.chestOpen,sprites.dungeon.chestClosed,sprites.dungeon.collectibleRedCrystal,sprites.dungeon.collectibleInsignia,sprites.dungeon.collectibleBlueCrystal], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
